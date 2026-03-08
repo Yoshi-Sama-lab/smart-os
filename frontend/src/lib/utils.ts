c9a4fa22810bc851455a2f31b3fe840ef05574cf
Yoshi-Sama-lab <144100310+Yoshi-Sama-lab@users.noreply.github.com>
2026-03-08 22:04:37 +0530

    Delete frontend directory

--- frontend/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx"; // 1. Import ClassValue
-import { twMerge } from "tailwind-merge";
-
-// 2. Add : ClassValue[] to the inputs parameter
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
